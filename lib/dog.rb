f54b6aea57710553e54724cd57451439dec85f5d
Stef Spears <spears.sj87@gmail.com>
2019-10-04 03:53:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,16 @@ class Dog
   
   @@all = []
   
-  def initialize()
+  def initialize(name)
+    @name = name 
     @@all << self 
   end 
   
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear_all 
+  end 
   
 end 
